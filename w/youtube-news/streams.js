b3023c7c4b724b176db868015ff0dce1a5d11c1b
Aravinth <55803552+Aravinth-Earth@users.noreply.github.com>
2026-08-18 22:03:41 +0530

    Add files from private repo to be published - 20260818220223 (#2099)
    Co-authored-by: github-actions <actions@github.com>

--- w/youtube-news/streams.js
+++ w/youtube-news/streams.js
@@ -1,15 +1,15 @@
 window.YTN_STREAMS = {
-  "generated_at": "2026-08-18T14:07:50+0530",
+  "generated_at": "2026-08-18T22:02:25+0530",
   "channels": [
     {
       "name": "News 18 Tamil Nadu",
       "handle": "@News18Tamilnadu",
       "streams": [
-        "5ENQ5m_2M9k",
-        "UbifklOHQ_g",
-        "9sc3It6jrOE",
+        "GhJ25KHYUGM",
+        "DN_lnYbU_FA",
+        "ksfl1RYBEYs",
         "g9rlCzIVFyY",
-        "ksfl1RYBEYs"
+        "E4ndYFfdlb8"
       ]
     },
     {
@@ -18,31 +18,26 @@ window.YTN_STREAMS = {
       "streams": [
         "4tKgfIvW3WQ",
         "Grkq4BdxjRQ",
-        "zsetxCzBVHs",
-        "HYmcQz1yLlE",
-        "a5WvofBvQPI"
+        "YkeNKe9kjds",
+        "1af_q3hlrLQ",
+        "J1rIIn351eI"
       ]
     },
     {
       "name": "News Tamil 24 x 7",
       "handle": "@NewsTamil24X7TV",
       "streams": [
-        "zPpmDrJ6CdE",
-        "dKPU-L1Be24",
-        "ok4LnGPTq5w",
-        "tn_6z9r-h0M",
-        "03eXtC79ssI"
+        "H9u3ds7jztk",
+        "3sYeR4RlKMs",
+        "gynWNinqmjw"
       ]
     },
     {
       "name": "Satiyam TV",
       "handle": "@SathiyamTV",
       "streams": [
-        "TCdb8x7Thl4",
-        "hOiG5PkB3uI",
-        "Jny_fWfqC6Y",
-        "-TTT2BEKVLI",
-        "98AWoPPH7V8"
+        "zQo4QC2T_T8",
+        "-8_V7EbnRiw"
       ]
     },
     {
@@ -50,20 +45,18 @@ window.YTN_STREAMS = {
       "handle": "@thanthitv",
       "streams": [
         "libuJrKpxEo",
-        "cn7PILWK4Qw",
         "0wbRkiYOGFY",
-        "6cnpQkDCr2I",
-        "Q5kxFX6O7ws"
+        "f0DunE1Jjj0",
+        "3vMq3SFAbms",
+        "_Lg0IUxUMi0"
       ]
     },
     {
       "name": "புதிய தலைமுறை | PuthiyathalaimuraiTV",
       "handle": "@PuthiyaThalaimuraiTV",
       "streams": [
-        "ghWiulNm0r0",
-        "g1hbOaDzHL4",
-        "_x4EXZdUg30",
-        "o4W3GOFjA08",
+        "gQoMrDS-8FQ",
+        "_xtjC93HGWQ",
         "hw7Fjh6mncQ"
       ]
     },
@@ -71,7 +64,6 @@ window.YTN_STREAMS = {
       "name": "Sun News Tamil",
       "handle": "@Sunnewstamil",
       "streams": [
-        "9v4uhsava88",
         "9M02G5c6x6w"
       ]
     },
@@ -86,8 +78,7 @@ window.YTN_STREAMS = {
       "name": "News J",
       "handle": "@NewsJ",
       "streams": [
-        "x1M7MOQ01AE",
-        "N8qI_W6oaI8"
+        "x1M7MOQ01AE"
       ]
     }
   ]

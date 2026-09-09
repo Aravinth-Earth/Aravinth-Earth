28bc9fe8af71e489d298021a187d1d814aa2813a
Aravinth <55803552+Aravinth-Earth@users.noreply.github.com>
2026-09-09 09:08:09 +0530

    Add files from private repo to be published - 20260909090647 (#2159)
    Co-authored-by: github-actions <actions@github.com>

--- w/youtube-news/streams.js
+++ w/youtube-news/streams.js
@@ -1,13 +1,13 @@
 window.YTN_STREAMS = {
-  "generated_at": "2026-09-09T00:48:04+0530",
+  "generated_at": "2026-09-09T09:06:50+0530",
   "channels": [
     {
       "name": "News 18 Tamil Nadu",
       "handle": "@News18Tamilnadu",
       "streams": [
-        "OjEBjMLC90I",
-        "o6-ODb78HPk",
-        "emF0sim8y4s",
+        "zAM_VWPPolQ",
+        "bqVQquDGSes",
+        "fiP1wKl7r6Q",
         "E4ndYFfdlb8"
       ]
     },
@@ -15,24 +15,28 @@ window.YTN_STREAMS = {
       "name": "Polimer News",
       "handle": "@PolimerNews",
       "streams": [
+        "MIfS695xEFU",
         "UfcRTCNUFQ8",
-        "o2jWKa2IJUs"
+        "xlRpiSU0xuE",
+        "Dbtss1hW2Bk",
+        "4mYR79yeWXU"
       ]
     },
     {
       "name": "News Tamil 24 x 7",
       "handle": "@NewsTamil24X7TV",
       "streams": [
-        "bEcW0ADdW6g",
-        "5LkipozJ4Kw",
-        "gynWNinqmjw",
-        "B5-NZJeKK_0"
+        "4WJNqMdHE7Y",
+        "naIJvJrymkE",
+        "Rb0p2dwajsU",
+        "gynWNinqmjw"
       ]
     },
     {
       "name": "Satiyam TV",
       "handle": "@SathiyamTV",
       "streams": [
+        "f0IT_kMU658",
         "-8_V7EbnRiw"
       ]
     },
@@ -40,7 +44,10 @@ window.YTN_STREAMS = {
       "name": "Thanthi TV",
       "handle": "@thanthitv",
       "streams": [
-        "cjN-A39bnh0",
+        "J58ci_2IRug",
+        "WHIGd91_bS0",
+        "7ist1Svkfms",
+        "eUbOMkuQ_-U",
         "_Lg0IUxUMi0"
       ]
     },
@@ -48,6 +55,8 @@ window.YTN_STREAMS = {
       "name": "புதிய தலைமுறை | PuthiyathalaimuraiTV",
       "handle": "@PuthiyaThalaimuraiTV",
       "streams": [
+        "Ql-J76PXdVs",
+        "1QvhhlDQw0s",
         "hw7Fjh6mncQ"
       ]
     },
@@ -55,6 +64,7 @@ window.YTN_STREAMS = {
       "name": "Sun News Tamil",
       "handle": "@Sunnewstamil",
       "streams": [
+        "RF0F3YwMsHg",
         "9M02G5c6x6w"
       ]
     },
